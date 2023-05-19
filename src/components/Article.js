3f083ff6c8f6d28c932ac4d391d0fee2cfaa48c8
John DH. Park <john.dh.park@icloud.com>
2023-05-19 14:13:32 -0700

    completed bonus feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 const Article = ({ title, date = "January 1, 1970", preview, minutes }) => {
+	let timeIndicators = "";
+
+	if (minutes < 30) {
+		const fives = Math.ceil(minutes / 5);
+		timeIndicators = "☕️".repeat(fives);
+	} else {
+		const tens = Math.ceil(minutes / 10);
+		timeIndicators = "🍱".repeat(tens);
+	}
+
 	return (
 		<article>
 			<h3>{title}</h3>
-			<small>{date}</small>
+			<small>
+				{date} {timeIndicators} {minutes} read
+			</small>
 			<p>{preview}</p>
 		</article>
 	);
